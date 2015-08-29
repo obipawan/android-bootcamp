145e08ca57453e712922f3b4ac0ee4225ff9a299
Pawan <pawan.kumar1@myntra.com>
2015-08-29 17:20:01 +0530

    .

--- app.js
+++ app.js
@@ -5,13 +5,47 @@ var app = require('koa')(),
 
 
 var config = require(__dirname + '/config.js');
-var feed = require('./feed.json');
+var feedv2 = require('./feedv2.json');
+var feed = require('./feedv1.1.json');
+var feedbefore = require('./feedv1.1before.json');
+var feedafter = require('./feedv1.1after.json');
+var feedzero = require('./feedempty.json');
+var androidConfig = require('./androidconfig.json');
 var port = process.env.PORT || config.koa.port || 3000;
 
 app.use(router(app));
 
 app.get('/lgp/v2/feed', function*(next){
-	this.body = JSON.stringify(feed);
+	this.body = JSON.stringify(feedv2);
+	yield next;
+});
+
+app.get('/lgp/v1.1/feed', function*(next){
+		if(this.query.before){
+			if(this.query.before == '1436459407001'){
+				this.body = JSON.stringify(feedbefore);
+				yield next;
+			}else{
+				this.body = JSON.stringify(feedzero);
+				yield next;
+			}
+		}else if(this.query.after){
+			console.log('here');
+			if(this.query.after == '1436459407020'){
+				this.body = JSON.stringify(feedafter);
+				yield next;
+			}else{
+				this.body = JSON.stringify(feedzero);
+				yield next;
+			}
+		}else{
+			this.body = JSON.stringify(feed);
+			yield next;
+		}
+});
+
+app.get('/config/android', function*(next){
+	this.body = JSON.stringify(androidConfig);
 	yield next;
 });
 
