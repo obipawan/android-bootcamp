14e037c5b3a8e8c8bb56930c40567d90fbbc4683
Pawan <pawan.kumar1@myntra.com>
2016-01-08 16:12:22 +0530

    .

--- app.js
+++ app.js
@@ -134,11 +134,11 @@ app.get('/layouts/android/feed', function*(next){
 	this.body = JSON.stringify(myxhotlist);
 });
 
-app.get('/lgp/v2.1/nav/android', function*(next){
+app.get('/lgp/v2.1/stream/nav', function*(next){
 	this.body = getFeed(nav);
 });
 
-app.get('/lgp/v2.1/carousel/android', function*(next){
+app.get('/lgp/v2.1/stream/carousel', function*(next){
 	this.body = getFeed(slideshow);
 });
 
